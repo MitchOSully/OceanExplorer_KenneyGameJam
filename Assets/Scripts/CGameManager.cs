ce01a9d0ce42a3ad965209abf23b89267a089a75
MitchOSully <mitchell.osullivan@live.com.au>
2023-07-22 17:36:24 +0800

    Debug reset button.
    - Press 'r' to reset the position of the player. Added this because the
      colliders sometimes don't work out and he phases through blocks, then
      gets stuck inside them.

---
+++ Assets/Scripts/CGameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGameManager : MonoBehaviour
+{
+    public GameObject m_player;
+    
+    private TarodevController.PlayerController      m_playerControllerLand;
+    private TarodevController.PlayerControllerWater m_playerControllerWater;
+
+    private void Start()
+    {
+        m_playerControllerLand = m_player.GetComponent<TarodevController.PlayerController>();
+        m_playerControllerWater = m_player.GetComponent<TarodevController.PlayerControllerWater>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            m_player.transform.position = new Vector3(0.64f, -1.36f, 0);
+            m_playerControllerLand.enabled = true;
+            m_playerControllerWater.enabled = false;
+        }
+    }
+}
