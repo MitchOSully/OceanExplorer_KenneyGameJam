cd35523812896b73de639425f5853459ed0a5a72
MitchOSully <mitchell.osullivan@live.com.au>
2023-07-29 11:24:36 +0800

    Particle effect when collecting coins

--- Assets/Scripts/CCollectible.cs
+++ Assets/Scripts/CCollectible.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class CCollectible : MonoBehaviour
 {
     public CGameManager m_gameManager;
+    public GameObject m_collectParticlePrefab;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            m_gameManager.IncreaseScore();
-            Destroy(gameObject);
+            Collect();
         }
+    }
+
+    public void Collect()
+    {
+        Instantiate(m_collectParticlePrefab, transform.position, transform.rotation);
+        //TO DO: play sound
+        m_gameManager.IncreaseScore();
+        Destroy(gameObject);
     }
 }
