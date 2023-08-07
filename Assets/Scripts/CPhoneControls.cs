ddbeee177b365ff773717a7cd7b609484469e4f4
MitchOSully <mitchell.osullivan@live.com.au>
2023-08-07 22:06:40 +0800

    Add land movement joystick
    Only left and right movement, and only on land.

---
+++ Assets/Scripts/CPhoneControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CPhoneControls : MonoBehaviour
+{
+    public Vector2 m_moveVector = Vector2.zero;
+
+    private InputActions m_input = null;
+    
+    private void Awake()
+    {
+        m_input = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        m_input.Enable();
+        m_input.Player.Movement.performed += OnMovementPerformed;
+        m_input.Player.Movement.canceled += OnMovementCanceled;
+    }
+
+    private void OnDisable()
+    {
+        m_input.Disable();
+        m_input.Player.Movement.performed -= OnMovementPerformed;
+        m_input.Player.Movement.canceled -= OnMovementCanceled;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        m_moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCanceled(InputAction.CallbackContext value)
+    {
+        m_moveVector = Vector2.zero;
+    }
+}
--- Assets/Scripts/Ultimate2DController/PlayerController.cs
+++ Assets/Scripts/Ultimate2DController/PlayerController.cs
@@ -21,7 +21,7 @@ namespace TarodevController {
 
         private Vector3 _lastPosition;
         private float _currentHorizontalSpeed, _currentVerticalSpeed;
-        private bool m_bRightButtonPressed = false;
+        public CPhoneControls m_phoneControls;// = new CPhoneControls();
 
         // This is horrible, but for some reason colliders are not fully established when update starts...
         private bool _active;
@@ -47,17 +47,12 @@ namespace TarodevController {
 
 
         #region Gather Input
-
-        public void MoveRightButtonDown()
-        {
-            m_bRightButtonPressed = true;
-        }
-
         private void GatherInput() {
+            Debug.Log(m_phoneControls.m_moveVector);
             Input = new FrameInput {
                 JumpDown = UnityEngine.Input.GetButtonDown("Jump"),
                 JumpUp = UnityEngine.Input.GetButtonUp("Jump"),
-                X = m_bRightButtonPressed ? 1 : 0, //UnityEngine.Input.GetAxisRaw("Horizontal"),
+                X = m_phoneControls.m_moveVector.x, //UnityEngine.Input.GetAxisRaw("Horizontal"),
                 Y = UnityEngine.Input.GetAxisRaw("Vertical")
             };
             if (Input.JumpDown) {
